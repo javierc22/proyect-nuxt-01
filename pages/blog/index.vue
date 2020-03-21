39a471b0d7f8c4159fc45b007253364b97d6a744
javierc22 <javier.ec.22@gmail.com>
2020-03-21 13:58:08 -0300

    Rutas dinámicas con Nuxt

---
+++ pages/blog/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <ul>
+      <li>Artículo #1</li>
+      <li>Artículo #2</li>
+      <li>Artículo #3</li>
+      <li>Artículo #4</li>
+      <li>Artículo #5</li>
+    </ul>
+  </div>
+</template>
