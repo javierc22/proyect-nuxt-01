d5830542e0375b9f8039cddaf063a54527c4c7bb
javierc22 <javier.ec.22@gmail.com>
2020-03-21 15:21:29 -0300

    Práctica. Página detalle Blog

--- pages/blog/_id.vue
+++ pages/blog/_id.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="container">
-    <h1>Detalle del artículo id: {{ $route.params.id }}</h1>
+    <div class="card">
+      <div class="card-body">
+        <h2>{{ articulo.title }}</h2>
+        <p class="small">{{ articulo.userId }}</p>
+        <p>{{ articulo.body }}</p>
+        <nuxt-link to="/blog" class="btn btn-primary">Atrás</nuxt-link>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      articulo: '',
+    }
+  },
+  async created(){
+    try {
+      const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${this.$route.params.id}`);
+      this.articulo = res.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+</script>
