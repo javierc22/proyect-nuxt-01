83912318260315ca8e01f950626ddb06c5bcafa9
javierc22 <javier.ec.22@gmail.com>
2020-03-29 21:29:44 -0300

    Fetch en Nuxt

--- pages/vuex/index.vue
+++ pages/vuex/index.vue
@@ -1,20 +1,37 @@
 <template>
   <div>
     <h2>Vuex</h2>
+    <ul>
+      <li v-for="(item, index) in tareas" :key="index">
+        {{ item.id }} - {{ item.nombre }}
+        <b-button class="btn-sm btn-warning">Editar</b-button>
+        <b-button class="btn-sm btn-danger">Eliminar</b-button>
+        <hr>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import { db } from '@/plugins/firebase'
+import { mapState } from "vuex";
 
 export default {
-  created(){
-    db.collection('tareas').get()
-      .then( query => {
-        query.forEach(element => {
-          console.log(element.data());
-        });
-      })
-  }
+  // fetch({ store }){
+  //   return db.collection('tareas').get()
+  //     .then( query => {
+  //       const tareas = [];
+  //       query.forEach(element => {
+  //         tareas.push(element.data())  
+  //       });
+  //       return store.commit('setTareas', tareas);
+  //     })
+  //     .catch(function(error){
+  //       console.log(error);
+  //     });
+  // }
+  computed: {
+    ...mapState(['tareas']),
+  },
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -1,11 +1,30 @@
-export const state = () => ({
+import { db } from "@/plugins/firebase";
 
+export const state = () => ({
+  tareas: '',
 })
 
 export const mutations = {
-
+  setTareas(state, payload){
+    console.log(payload);
+    state.tareas = payload;
+  }
 }
 
 export const actions = {
-  
+  nuxtServerInit({ commit }, { req }){
+    return db.collection('tareas').get()
+      .then( query => {
+        const tareas = [];
+        query.forEach( element => {
+          let tarea = element.data();
+          tarea.id = element.id;
+          tareas.push(tarea);
+        });
+        return commit('setTareas', tareas);
+      })
+      .catch(function(error){
+        console.log(error);
+      });
+  }
 }
